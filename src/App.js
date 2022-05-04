3ea5c402ce0432fde32e3417553fb3244a166ed1
Kirill Tchentsov <kirill.develops@gmail.com>
2022-05-04 18:23:37 -0400

    created basic route structure withen app & added BrowserRouter to index

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import './styles/global.scss';
+import Nav from "./components/Nav/Nav";
+import Home from "./pages/Home/Home";
+import Universities from "./pages/Universities/Universities";
+import PostalLookup from "./pages/PostalLookup/PostalLookup";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-     Hello World
+      <Nav />
+      <Routes>
+        <Route
+          path='/'
+          element={(<Home />)}
+        />
+        <Route
+          path='/universities'
+          element={(<Universities />)}
+        />
+         <Route
+          path='/postal-lookup'
+          element={(<PostalLookup />)}
+        />
+      </Routes>
+      Hello World!
     </div>
   );
 };
