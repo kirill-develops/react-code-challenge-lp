905756c0c3e7f1a8158e4e4b325c673de2cc03af
Kirill Tchentsov <kirill.develops@gmail.com>
2022-05-05 20:02:22 -0400

    added onFormSubmit function to AddPostModal

--- src/components/AddPostModal/AddPostModal.jsx
+++ src/components/AddPostModal/AddPostModal.jsx
@@ -1,16 +1,36 @@
 import './AddPostModal.scss';
 import React, { useReducer, useState } from 'react';
+import { addPost } from '../../features/posts/postsActions';
+import { customAlphabet } from 'nanoid';
+import { useDispatch } from 'react-redux';
+
+const nanoid = customAlphabet('1234567890', 12)
 
 const AddPostModal = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [newPost, toggleNewPost] = useReducer(checked => !checked, false);
 
+  const dispatch = useDispatch();
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    if (title && content) {
+      const postObj = {
+        title: title,
+        body: content,
+        userId: nanoid(),
+      };
+      dispatch(addPost(postObj));
+    }
+  }
+
   return newPost ? (
     <section className='new-post__overlay'>
       <div className='new-post'>
         <h2>Add a New Post</h2>
-        <form className='new-post__form'>
+        <form onSubmit={onFormSubmit} className='new-post__form'>
           <label htmlFor="postTitle">Post Title:</label>
           <input
             type="text"
@@ -27,7 +47,7 @@ const AddPostModal = () => {
             onChange={e => setContent(e.target.value)}
           />
           <button
-            type="button"
+            type="submit"
             className='new-post__button'
           >Save Post</button>
           <button
